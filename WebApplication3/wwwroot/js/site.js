d4c2a9edee952e0fa13695d5411c656eef9d2646
BootlegYouki <darrenjade24@gmail.com>
2025-03-06 16:37:36 +0800

    Add project files.

---
+++ WebApplication3/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const products = document.querySelectorAll('.product');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.2 });
+
+    products.forEach(product => observer.observe(product));
+});
+
+$(document).ready(function () {
+    $('.navbar-toggler').on('click', function () {
+        var $icon = $('#bar');
+        $icon.text($icon.text().trim() === "menu" ? "close" : "menu");
+    });
+});
